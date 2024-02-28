169fb014d88ef17da7c1b7bb5726f06d9f71b93a
luxeave <luxeaves@gmail.com>
2024-02-28 21:50:37 +0700

    Add publisher module with dependencies and service definition

---
+++ src/publisher/src/lib.rs
@@ -0,0 +1,51 @@
+use candid::{CandidType, Principal};
+use ic_cdk::{query, update};
+use serde::Deserialize;
+use std::cell::RefCell;
+use std::collections::BTreeMap;
+
+type SubscriberStore = BTreeMap<Principal, Subscriber>;
+
+thread_local! {
+    static SUBSCRIBERS: RefCell<SubscriberStore> = RefCell::default();
+}
+
+#[derive(Clone, Debug, CandidType, Deserialize)]
+struct Counter {
+    topic: String,
+    value: u64,
+}
+
+#[derive(Clone, Debug, CandidType, Deserialize)]
+struct Subscriber {
+    topic: String,
+}
+
+#[update]
+fn subscribe(subscriber: Subscriber) {
+    let subscriber_principal_id = ic_cdk::caller();
+    SUBSCRIBERS.with(|subscribers| {
+        subscribers
+            .borrow_mut()
+            .insert(subscriber_principal_id, subscriber)
+    });
+}
+
+#[update]
+async fn publish(counter: Counter) {
+    println!("Incoming counter: {:?}", counter);
+    SUBSCRIBERS.with(|subscribers| {
+        for (k, v) in subscribers.borrow().iter() {
+            if v.topic == counter.topic {
+                let _call_result: Result<(), _> =
+                    ic_cdk::notify(*k, "update_count", (&counter,));
+            }
+        }
+    });    
+}
+
+#[query]
+fn get_id() -> String {
+    ic_cdk::api::id().to_text()
+}
+
