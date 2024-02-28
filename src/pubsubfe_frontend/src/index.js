fc26319fabe9d38865636852aa4f1f8e5791bb9b
luxeave <luxeaves@gmail.com>
2024-02-28 21:52:41 +0700

    Update CSS styles and HTML structure for PubSub frontend

--- src/pubsubfe_frontend/src/index.js
+++ src/pubsubfe_frontend/src/index.js
@@ -1,19 +1,69 @@
-import { pubsubfe_backend } from "../../declarations/pubsubfe_backend";
+import { publisher } from '../../declarations/publisher';
+import { subscriber } from '../../declarations/subscriber';
+import { Principal } from '@dfinity/principal';
 
-document.querySelector("form").addEventListener("submit", async (e) => {
+window.addEventListener("load", async () => {
+  //console.log("finished loading");
+  await update_publisher();
+  await update_subscriber();
+});
+
+document.querySelector("#form-subscribe").addEventListener("submit", async (e) => {
+  // override the default form submission behavior
   e.preventDefault();
-  const button = e.target.querySelector("button");
+  // console.log("Submitted");
 
-  const name = document.getElementById("name").value.toString();
-
+  const button = e.target.querySelector("#submit-btn-subscribe");
   button.setAttribute("disabled", true);
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await pubsubfe_backend.greet(name);
+  const inputPubId = document.getElementById("input-pubid").value;
+  const topic = document.getElementById("input-topic").value;
+  
 
+  if (inputPubId.length != 0) {
+    const principal = Principal.fromText(inputPubId);
+    await subscriber.setup_subscribe(principal, topic);
+  }
+  
+  button.removeAttribute("disabled");
+  window.location.reload();
+} );
+
+document.querySelector("#form-publish").addEventListener("submit", async (e) => {
+  // override the default form submission behavior
+  e.preventDefault();
+  // console.log("Submitted");
+
+  const button = e.target.querySelector("#submit-btn-publish");
+  button.setAttribute("disabled", true);
+
+  const inputData = document.getElementById("input-data").value;
+  const topic = document.getElementById("input-topic2").value;
+  
+  try {
+    if (inputData.length != 0) {
+      let counter = {
+        topic: topic,
+        value: parseInt(inputData)
+      };
+      await publisher.publish(counter);
+      console.log("Publish successful");
+    }
+  } catch (error) {
+    console.error("Publish failed: ", error);
+  }
+  
   button.removeAttribute("disabled");
 
-  document.getElementById("greeting").innerText = greeting;
+  window.location.reload();
+} );
 
-  return false;
-});
+async function update_publisher() {
+  const pub_id = await publisher.get_id();
+  document.getElementById("pubid").innerText = pub_id; 
+}
+
+async function update_subscriber() {
+  const data = await subscriber.get_count();
+  document.getElementById("data").innerText = data;
+}
